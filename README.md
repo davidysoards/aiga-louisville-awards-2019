5a794d04e716fec58d8fb4ec9cf207df768af636
David Soards <davidysoards@gmail.com>
2020-02-14 02:42:27 -0500

    tweaks, og-image, logo in header

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# 11ty-tailwind-simple-starter
 # aiga-louisville-awards-2019
--- src/site/_includes/js/main.js
+++ src/site/_includes/js/main.js
@@ -1,5 +1,5 @@
 function Gallery(gallery) {
-  const images = document.querySelectorAll('.winnerImage');
+  const images = gallery.querySelectorAll('.winnerImage');
   const modal = document.querySelector('.modal');
   const prevButton = modal.querySelector('.prev');
   const nextButton = modal.querySelector('.next');
@@ -38,8 +38,18 @@ function Gallery(gallery) {
       return;
     }
     // update the modal with this info
-    console.log(el.dataset.full);
-    modal.querySelector('.modalImage').src = el.dataset.full;
+    console.log(el.dataset.full, el.dataset.video);
+    if (el.dataset.video) {
+      modal.querySelector('.modalVideo').src = el.dataset.video;
+      modal.querySelector('.modalImage').classList.add('hidden');
+      modal.querySelector('.modalVideo').classList.remove('hidden');
+    } else {
+      modal.querySelector('.modalImage').src = el.dataset.full;
+      modal.querySelector('.modalVideo').classList.add('hidden');
+      modal.querySelector('.modalVideo').src = '';
+      modal.querySelector('.modalImage').classList.remove('hidden');
+    }
+
     modal.querySelector('.modalTitle').textContent = el.title;
     modal.querySelector('.modalGroup').textContent = el.dataset.group;
     modal.querySelector('.modalCategory').textContent = el.dataset.category;
@@ -76,6 +86,7 @@ function Gallery(gallery) {
   modal.addEventListener('click', handleClickOutside);
 }
 
+const gallery4 = Gallery(document.querySelector('.gallery-4'));
 const gallery1 = Gallery(document.querySelector('.gallery-1'));
 const gallery2 = Gallery(document.querySelector('.gallery-2'));
 const gallery3 = Gallery(document.querySelector('.gallery-3'));
